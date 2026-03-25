feb736c588786ea2dc877f481d5f7cc4a1312717
nanarecipes <nanarecipes@proton.me>
2026-03-25 18:44:00 +0700

    Create animalDB.js

---
+++ animalDB.js
@@ -0,0 +1,19 @@
+<script>
+const animalDB={
+"Whale":"a massive blue whale with enormous body and slow graceful movement",
+"Dolphin":"a playful dolphin swimming quickly through the water",
+"Shark":"a powerful shark with sharp teeth",
+"Sea Turtle":"a graceful sea turtle gliding underwater",
+"Jellyfish":"a glowing jellyfish floating gently",
+"Megalodon":"a gigantic prehistoric shark with enormous jaws and terrifying presence",
+"Giant Manta Ray":"a massive manta ray with wide wingspan gliding gracefully",
+"Giant Octopus":"a huge octopus with long tentacles moving intelligently",
+"Mola Mola (Ocean Sunfish)":"a giant ocean sunfish with flat massive body drifting slowly",
+"Sperm Whale":"a gigantic deep sea whale with a massive head and powerful presence",
+"Colossal Squid":"an enormous squid with long tentacles and deep sea mystery",
+"Giant Moray Eel":"a large moray eel emerging from rocks with sharp teeth",
+"Whale Shark":"a gigantic gentle whale shark swimming slowly",
+"Hammerhead Shark":"a large hammerhead shark with unique head shape",
+"Giant Sea Serpent":"a mythical massive sea serpent moving through deep water"
+}
+</script>
